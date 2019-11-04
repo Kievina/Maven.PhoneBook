68bc5c0a0294ca71a04f3d0537bc561f1181ce33
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-03 20:57:27 -0500

    only some tests pass

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,13 +1,17 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private Map<String, List<String>> map;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = new HashMap<>();
     }
 
     public PhoneBook() {
@@ -15,31 +19,45 @@ public class PhoneBook {
     }
 
     public void add(String name, String phoneNumber) {
+        map.put(name, Arrays.asList(phoneNumber));
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        map.put(name, Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return map.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        //String keyLookup = "";
+        for(Map.Entry<String, List<String>> pair: map.entrySet()) {
+            if(map.get(pair.getKey()).equals(phoneNumber))
+                return pair.getKey();
+
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        ArrayList<String> keyList = new ArrayList<String>();
+        for(Map.Entry<String, List<String>> pair: map.entrySet()) {
+            keyList.add(pair.getKey());
+        }
+        //Collections.sort(map);
+        return keyList;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return map;
     }
 }
